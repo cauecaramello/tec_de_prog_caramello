36e8a26f2651ee1b0a33bd52730cf11aeba44d8c
Caramello <cauedangertm@hotmail.com>
2024-02-29 16:18:09 -0300

    Criação do projeto java

---
+++ src/br/edu/fatecpg/tecprog/controleversao/principal.java
@@ -0,0 +1,10 @@
+package br.edu.fatecpg.tecprog.controleversao;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá Mundo!");
+	}
+
+}
