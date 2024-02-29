024ab9db71a272c97eaac866355333ebcdb9b66d
Caramello <cauedangertm@hotmail.com>
2024-02-29 16:34:00 -0300

    Criação da classe Somar

---
+++ src/br/edu/fatecpg/tecprog/controleversao/Somar.java
@@ -0,0 +1,8 @@
+package br.edu.fatecpg.tecprog.controleversao;
+
+public class Somar {
+	public double soma(double num1, double num2) {
+		return num1 + num2;
+	}
+	
+}
--- src/br/edu/fatecpg/tecprog/controleversao/principal.java
+++ src/br/edu/fatecpg/tecprog/controleversao/principal.java
@@ -5,6 +5,10 @@ public class principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Olá Mundo!");
+		
+		Somar soma = new Somar();
+		System.out.println(calc.soma(20, 10));
+		
 	}
 
 }
